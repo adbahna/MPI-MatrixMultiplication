e11b60a29808b8a2b1970accfb2021fdb2bd09b6
adbahna <alexbahna@gmail.com>
2018-11-27 19:28:16 -0500

    Implemented single-processor Matrix-Matrix multiplication with stdout timing

--- matrix_mult.cpp
+++ matrix_mult.cpp
@@ -10,39 +10,52 @@ int main(int argc, char ** argv)
 {
 	MPI_Init(&argc, &argv);
 
-	int i, j, procCount, rank, rootProc, blockSize; 
-	double start_time, end_time;
+	int i, j, k, 
+		procCount, rank, rootProc, blockSize; 
+	double startTime, endTime, tempSum = 0; 
 	MPI_Comm c = MPI_COMM_WORLD;
 
-	MPI_Comm_size(c, &pCount);
+	MPI_Comm_size(c, &procCount);
 	MPI_Comm_rank(c, &rank);
 	rootProc = sqrt(procCount); 
-	blockSize = (128 / rootProc); 
+	blockSize = (N / rootProc); 
 
-	double A[128][128];
-	double B[128][128];
-	double C[128][128];
-	
+	double A[N][N];
+	double B[N][N];
+	double C[N][N];
+
 	if (rank == 0)
 	{
-		for (i = 0; i < N; i++) {
-        	for (j = 0; j < N; j++) {
+		for (i = 0; i < N; i++) 
+        	for (j = 0; j < N; j++) 
             	A[i][j] = ((double) (rand()) / double(RAND_MAX)) * (1 + 1) - 1;
-        	}
-    	}
 
-		for (i = 0; i < N; i++) {
-        	for (j = 0; j < N; j++) {
+		for (i = 0; i < N; i++) 
+        	for (j = 0; j < N; j++) 
             	B[i][j] = ((double) (rand()) / double(RAND_MAX)) * (1 + 1) - 1;
-        	}
-    	}
     	
-		for (i = 0; i < N; i++) {
-        	for (j = 0; j < N; j++) {
+		for (i = 0; i < N; i++)
+        	for (j = 0; j < N; j++) 
             	C[i][j] = 0;
-			}
-    	}
 
+		if (procCount == 1)
+		{
+			startTime = MPI_Wtime(); 
+			for (i = 0; i < N; i++)
+			{
+				for (j = 0; j < N; j++)
+				{
+					for (k = 0; k < N; k++)
+					{
+						tempSum += (A[i][k] * B[k][j]); 
+					}
+					C[i][j] = tempSum; 
+				}
+			}
+			endTime = MPI_Wtime();
+
+			printf("Elapsed Time (On Single Processor): %f\n", endTime - startTime); 
+		}
 	}
 
 	MPI_Finalize(); 
